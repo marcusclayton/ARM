6cd169072e40db0387fac220b8b60e64c275febc
Marcus <marcusaclayton@gmail.com>
2016-11-01 23:40:36 -0700

    Added logic to display env var agent_name

--- 100-simple-vm/test.ps1
+++ 100-simple-vm/test.ps1
@@ -1,16 +1,28 @@
-#Check Module Path: Before
-Write-Host "$($env:PSModulePath.Split(';'))" -ForegroundColor Green
+<#$hash = @{
+    AGENT_MACHINE_NAME = $env:AGENT_MACHINE_NAME
+    AGENT_ID
+    AGENT_NAME
 
+}
+
+$blob = @"
+$env:AGENT_MACHINE_NAME
+$env:
+"@
+#>
+
+Write-Warning "AGENT_NAME is: $env:AGENT_NAME"
+
+<#if($env:AGENT_NAME -like '*HOSTED*'){
+    Write-Warning
+    
+}
+#>
+
+<#
 $azure1 = ";" + "C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ResourceManager\AzureResourceManager"
 $azure2 = ";" + "C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\Storage"
 $azure3 = ";" + "C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ServiceManagement"
 
 $env:PSModulePath=$env:PSModulePath+"$azure1"+"$azure2"+"$azure3"
-
-#Check Module Path: After
-Write-Host "$($env:PSModulePath.Split(';'))" -ForegroundColor Green
-
-$groups = Get-AzureRmResourceGroup
-Write-Host "$($groups.count)" -ForegroundColor Cyan
-
-Write-Warning "$env:AGENT_NAME"
+#>
