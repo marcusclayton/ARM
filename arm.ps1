6cf542088e16f4f880cf322cb4a3d0a6f45548f5
Marcus <marcusaclayton@gmail.com>
2016-11-01 18:42:32 -0700

    adding code from ARM template

--- arm.ps1
+++ arm.ps1
@@ -1,4 +1,15 @@
-Get-Module azure* -listavailable
+$modules = Get-Module azure* -listavailable
+$modules.count
+
+Import-Module Azure -ErrorAction SilentlyContinue
+
+try {
+    [Microsoft.Azure.Common.Authentication.AzureSession]::ClientFactory.AddUserAgent("VSAzureTools-$UI$($host.name)".replace(" ","_"), "2.9.5")
+} catch { }
+
+Set-StrictMode -Version 3
+
+
 
 <#Import-Module azure -verbose
 
