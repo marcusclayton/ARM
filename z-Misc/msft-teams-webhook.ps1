8f517facb4a8d054448a0f657fdb4f124b33a1b1
Marcus <marcusaclayton@gmail.com>
2016-11-03 11:11:12 -0700

    Corrected script parameter typo.

--- z-Misc/msft-teams-webhook.ps1
+++ z-Misc/msft-teams-webhook.ps1
@@ -1,37 +1,42 @@
-﻿params(
-    $uri
-)
-
-# these values would be retrieved from or set by an application
-$status = 'success'
-$fact1 = 'All tests passed.'
-$fact2 = 'None'
-
-$body = ConvertTo-Json -Depth 4 @{
-    title = 'New Build Notification'
-    text = "A build completed with status $status"
-    sections = @(
-        @{
-            activityTitle = 'Build'
-            activitySubtitle = 'automated test platform'
-            activityText = 'A change was evaluated and new results are available.'
-            activityImage = 'https://mcautomationgitresources.blob.core.windows.net/images/GitHub-Mark-64px.png' # this value would be a path to a nice image you would like to display in notifications
-        },
-        @{
-            title = 'Details'
-            facts = @(
-                @{
-                name = 'Unit Tests'
-                value = $fact1
-                },
-                @{
-                name = 'Integration Tests'
-                value = $fact2
-                }
-            )
-        }
+﻿function Invoke-MsftTeamsWebhook{
+    Param(
+        $uri
     )
+
+    # these values would be retrieved from or set by an application
+    $status = 'success'
+    $fact1 = 'All tests passed.'
+    $fact2 = 'None'
+
+    $body = ConvertTo-Json -Depth 4 @{
+        title = 'New Build Notification'
+        text = "A build completed with status $status"
+        sections = @(
+            @{
+                activityTitle = 'Build'
+                activitySubtitle = 'automated test platform'
+                activityText = 'A change was evaluated and new results are available.'
+                activityImage = 'https://mcautomationgitresources.blob.core.windows.net/images/GitHub-Mark-64px.png' # this value would be a path to a nice image you would like to display in notifications
+            },
+            @{
+                title = 'Details'
+                facts = @(
+                    @{
+                    name = 'Unit Tests'
+                    value = $fact1
+                    },
+                    @{
+                    name = 'Integration Tests'
+                    value = $fact2
+                    }
+                )
+            }
+        )
+    }
+
+
+    Invoke-RestMethod -uri $uri -Method Post -body $body -ContentType 'application/json'
+
 }
 
-
-Invoke-RestMethod -uri $uri -Method Post -body $body -ContentType 'application/json'
+Invoke-MsftTeamsWebhook -uri $uri
